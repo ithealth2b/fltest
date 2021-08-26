95aa044360e3ce08de27a893224509b8ed28312e
Alexander Yarosh <silver_glow@mail.ru>
2021-08-26 17:29:39 +0300

    start repository

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+
+@app.route('/new')
+def newword() -> str:
+    return 'new word'
+
+
+def main():
+    app.run(host='0.0.0.0', port=80)
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests.py
@@ -0,0 +1,20 @@
+import unittest
+import main as tested_app
+
+
+class FlaskTest(unittest.TestCase):
+    def setUp(self):
+        tested_app.app.config['TESTING'] = True
+        self.app = tested_app.app.test_client()
+
+    def test_get_hello(self):
+        r = self.app.get('/')
+        self.assertEqual(r.data, b'Hello, World!')
+
+    def test_get_new(self):
+        r = self.app.get('/new')
+        self.assertEqual(r.data, b'new word')
+
+
+if __name__ == '__main__':
+    unittest.main()
